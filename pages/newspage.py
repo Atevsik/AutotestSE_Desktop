a98113653800dbf7c0745da7fd15f05bf85e4908
Nick <nick.khudakov@gmail.com>
2025-04-02 16:01:16 +0300

    Второй тест

--- pages/newspage.py
+++ pages/newspage.py
@@ -1,73 +1,149 @@
 from time import sleep
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+import logging
+import time
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class NewsPage:
 
     def __init__(self, browser):
         self.browser = browser
+        self.wait = WebDriverWait(self.browser, 10)
+        self.original_window = None
 
 
     def open(self):
         self.browser.get('https://www.sport-express.ru/news/')
         sleep(6)
 
-    def logo_news(self):
-        logo_news = self.browser.find_element(By.XPATH, '//div[@class="se-logo"]')
-
     def h1_news(self):
-        h1_news = self.browser.find_element(By.XPATH, '//h1[contains(text(),"Весь спорт.")]')
+        try:
+            h1_news = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,'//h1[contains(text(),"Весь спорт.")]')))
+            logging.info("Заголовок найден и виден")
+            return h1_news
+        except TimeoutException:
+            logging.info("Заголовок не найден")
+            return None
 
-    def button_glvn(self, count):
-        button_glvn = self.browser.find_elements(By.XPATH, '//div[@class="se-material-list-filter__button-holder"]')
-        assert len(button_glvn) == count
-
-    def button_glvn_click(self):
-        button_glvn_click = self.browser.find_element(By.XPATH, '//a[contains(text(),"Главные")]')
-        button_glvn_click.click()
-        assert self.browser.current_url == 'https://www.sport-express.ru/news/?isEditorialChoice=1', "Неверный урл Главные кнпк"
+    def button_main(self):
+        try:
+            button1 = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,'//a[contains(text(),"Главные")]')))
+            logging.info("Кнопка найдена и видна")
+            button1.click()
+            assert self.browser.current_url == 'https://www.sport-express.ru/news/?isEditorialChoice=1', "Неверный урл Главные кнопки"
+            return button1
+        except TimeoutException:
+            logging.info("Кнопка не найден")
+            return None
 
     def button_readers(self):
-        button_readers = self.browser.find_element(By.XPATH, '//a[contains(text(),"Выбор читателей")]')
-        button_readers.click()
-        assert self.browser.current_url == 'https://www.sport-express.ru/news/?isHot=1', "Не верный урл Выбор читателей"
+        try:
+            button2 = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,'//a[contains(text(),"Выбор читателей")]')))
+            logging.info("Кнопка найдена и видна")
+            button2.click()
+            assert self.browser.current_url == 'https://www.sport-express.ru/news/?isHot=1', "Не верный урл Выбор читателей"
+            return button2
+        except TimeoutException:
+            logging.info("Заголовок не найден")
+            return None
 
     def button_exclusive(self):
-        button_exclusive = self.browser.find_element(By.XPATH, '//a[contains(text(),"Эксклюзив")]')
-        button_exclusive.click()
-        assert self.browser.current_url == 'https://www.sport-express.ru/news/?isExclusive=1', "Не верный урл кнопки эксклюз"
+        try:
+            button3 = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//a[contains(text(),'Эксклюзив')]")))
+            logging.info("Кнопка найдена и видна")
+            button3.click()
+            assert self.browser.current_url == 'https://www.sport-express.ru/news/?isExclusive=1', "Не верный урл кнопки эксклюз"
+            return button3
+        except TimeoutException:
+            logging.info("Заголовок не найден")
+            return None
 
-
-    def vid_materiala_button(self):
-        vid_materiala_button = self.browser.find_elements(By.XPATH, '//div[@class="se-material-filter__top-left"]')
-
-    def vid_sporta_button(self):
-        vid_sporta_button = self.browser.find_elements(By.XPATH, '//div[@class="se-material-filter-menu"]')
+    def reviews_vid(self):
+        try:
+            reviews_vid = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//a[@class='se-material-filter-headline__item']")))
+            logging.info("Кнопка статьи найдена и видна")
+            reviews_vid.click()
+            assert self.browser.current_url == 'https://www.sport-express.ru/reviews/', "Не правильная кнопка статьи"
+            self.browser.back()
+            return reviews_vid
+        except TimeoutException:
+            logging.info("Кнопка не найдена")
+            return None
 
     def football_button(self):
-        football_button = self.browser.find_element(By.XPATH,'''//a[contains(@class,'se-material-filter-menu__item-button')][contains(text(),"Футбол")]''')
-        football_button.click()
-        assert self.browser.current_url == 'https://www.sport-express.ru/football/news/', "OSHIBKA УРЛА"
+        try:
+            football_button = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,'''//a[contains(@class,'se-material-filter-menu__item-button')][contains(text(),"Футбол")]''')))
+            logging.info("Кнопка найдена и видна")
+            football_button.click()
+            assert self.browser.current_url == 'https://www.sport-express.ru/football/news/', "Не правильный урл кнопки Футбол"
+            return football_button
+        except TimeoutException:
+            logging.info("Кнопка не найдена")
+            return None
 
-    def h1_footbal_news(self):
-        h1_footbal_news = self.browser.find_element(By.XPATH,'//h1[contains(text(),"Новости футбола")]')
+    def h1_football_news(self):
+        try:
+            h1_football_news = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,'//h1[contains(text(),"Новости футбола")]')))
+            logging.info("Кнопка найдена и видна")
+            return h1_football_news
+        except TimeoutException:
+            logging.info("Кнопка не найдена")
+            return None
 
-    def spisok_materialov(self):
-        spisok_materialov = self.browser.find_element(By.XPATH, '//div[@class="se-news-list-page__items"]')
+    def reviews(self,expected_count=30):
+        try:
+            reviews = self.wait.until(
+                EC.visibility_of_all_elements_located((By.XPATH,"//div[@class='se-news-list-page__item']")))
+            actual_count = len(reviews)
+            if actual_count == expected_count:
+                logging.info(f"Найдено {actual_count} статей (ожидалось {expected_count})")
+                return True
+            else:
+                logging.warning(f"Кол-во статей:{actual_count} (ожидалось {expected_count})")
+                return False
+        except TimeoutException:
+            logging.error("Не удалось загрузить список статей")
+            return False
 
     def cup_selector(self):
-        cup_selector_click = self.browser.find_element(By.XPATH, '//div[@class="se-select se-select--1col se-select--selected se-select--theme-sky"]')
-        cup_selector_click.click()
+        try:
+            cup_selector = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,'//div[@class="se-select se-select--1col se-select--selected se-select--theme-sky"]')))
+            logging.info("Селектор найден и виден")
+            cup_selector.click()
+            return cup_selector
+        except TimeoutException:
+            logging.info("Селектор не найден")
+            return None
 
-    def cup_selector_click(self):
-        cup_selector_click = self.browser.find_element(By.XPATH, '//div[@class="se-select__item"][7]')
-        cup_selector_click.click()
-        assert self.browser.current_url == 'https://www.sport-express.ru/football/world/news/'
+    def cup_rpl(self):
+        try:
+            cup_rpl = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//div[@class='se-select__items']//div[3]")))
+            logging.info("Селектор найден и виден")
+            cup_rpl.click()
+            assert self.browser.current_url == 'https://www.sport-express.ru/football/rfpl/news/', "Не правильный турнир"
+            return cup_rpl
+        except TimeoutException:
+            logging.info("Селектор не найден")
+            return None
 
-    def calendar_footbal(self):
-        calendar_footbal = self.browser.find_element(By.XPATH, '//div[@class="se-calendar-button"]')
-        calendar_footbal.click()
-
-    def calendar_footbal_click(self):
-        calendar_footbal_click = self.browser.find_element(By.XPATH, '//abbr[@aria-label="17 декабря 2024 г."]')
-        calendar_footbal_click.click()
-        assert self.browser.current_url == 'https://www.sport-express.ru/football/world/news/?publicationDate=17.12.2024'
+    def reklama_block(self):
+        try:
+            reklama_block = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//div[@id='adfox_15645683733586888']")))
+            logging.info("Реклама найдена")
+            return reklama_block
+        except TimeoutException:
+            logging.info("Реклама не найдена")
+            return None
--- test/test_4656.py
+++ test/test_4656.py
@@ -1,19 +1,51 @@
 from selenium.webdriver.common.by import By
 from pages.newspage import NewsPage
+import logging
 
 def test_newspage(browser):
     newspage = NewsPage(browser)
     newspage.open()
-    newspage.logo_news()
-    newspage.button_glvn(3)
-    newspage.button_glvn_click()
-    newspage.button_exclusive()
-    newspage.button_readers()
-    newspage.vid_materiala_button()
-    newspage.vid_sporta_button()
-    newspage.football_button()
-    newspage.h1_footbal_news()
-    newspage.spisok_materialov()
-    newspage.cup_selector()
-    newspage.calendar_footbal()
 
+    h1 = newspage.h1_news()
+    assert h1 is not None, "Заголовок не найден"
+    logging.info("Заголовок ОК")
+
+    button1 = newspage.button_main()
+    assert button1 is not None,"Кнопка Главная не найдена"
+    logging.info("Кнопка ОК ")
+
+    button2 = newspage.button_readers()
+    assert button2 is not None,"Кнопка Читатели не найдена"
+    logging.info("Кнопка ОК ")
+
+    button3 = newspage.button_exclusive()
+    assert button3 is not None,"Кнопка Эксклюзив не найдена"
+    logging.info("Кнопка ОК ")
+
+    reviews = newspage.reviews_vid()
+    assert reviews is not None,"Кнопка не найдена"
+    logging.info("Кнопка ОК")
+
+    football = newspage.football_button()
+    assert football is not None, "Кнопка Футбол, не найдена"
+    logging.info("Кнопка ОК")
+
+    h1_football = newspage.h1_football_news()
+    assert h1_football is not None,"Заголовок футбола не найден"
+    logging.info("Заголовок ОК")
+
+    newspage.reviews()
+    assert newspage.reviews(expected_count=30), "Кол-во статей не соответствует ожидаемому"
+    logging.info("Кол-во статей ОК")
+
+    selector = newspage.cup_selector()
+    assert selector is not None,"Слектор не найден"
+    logging.info("Селкетор ОК")
+
+    rpl = newspage.cup_rpl()
+    assert rpl is not None,"РПЛ не найден"
+    logging.info("РПЛ ОК")
+
+    reklama = newspage.reklama_block()
+    assert  reklama is not None, "Реклама не найдена"
+    logging.info("Реклама ОК")
