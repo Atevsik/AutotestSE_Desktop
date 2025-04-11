9199bce3d03d7f6ddf5420435bb63fa6ade4666f
Nick <nick.khudakov@gmail.com>
2025-04-11 14:49:30 +0300

    11fix

--- pages/detalvideo.py
+++ pages/detalvideo.py
@@ -1,43 +1,84 @@
-from selenium.webdriver.common.by import By
 from time import sleep
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains
+import logging
+
 
 class DetalVideo:
 
     def __init__(self, browser):
         self.browser = browser
+        self.wait = WebDriverWait(self.browser, 15)
+        self.original_window = None
+
 
     def open_detal_video(self):
         self.browser.get('https://www.sport-express.ru/hockey/khl/videoreports/aleksandr-hmelevskiy-vylet-salavata-seriya-s-traktorom-buduschee-v-klube-intervyu-2188513/')
-        sleep(6)
 
     def menu_nadlogo(self):
-        menu_nadlogo = self.browser.find_element(By.XPATH, '//div[@class="se-menu-subtop se-menu-subtop--breadcrumb"]')
+        try:
+            menu_nadlogo = self.wait.until(
+                EC.visibility_of_element_located(
+                    (By.XPATH, '//div[@class="se-menu-subtop se-menu-subtop--breadcrumb"]')))
+            logging.info("Меню найдено и видно")
+            return menu_nadlogo
+        except TimeoutException:
+            return None
 
-    def podval(self):
-        podval = self.browser.find_element(By.XPATH, '//footer[@class="se-footer"]')
+    def h1(self):
+        try:
+            h1 = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//h1[contains(text(),'Хмелевский: вылет «Салавата», серия с «Трактором»,')]")))
+            logging.info("Заголовок найден")
+            return h1
+        except TimeoutException:
+            return None
 
-    def eshe(self):
-        eshe = self.browser.find_element(By.XPATH, '//div[@class="se-menu-subtop__link"]')
-        eshe.click()
+    def rekalama(self):
+        try:
+            reklama = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//div[@id='adfox_15645683733586888']")))
+            logging.info("Реклама найдена")
+            return reklama
+        except TimeoutException:
+            return None
 
-    def perexod_stadium(self):
-        perexod_stadium = self.browser.find_element(By.XPATH,'''//div[@class='se-menu-subtop-popup__link']//a[contains(text(),"Стадионы")]''')
-        perexod_stadium.click()
-        assert self.browser.current_url == 'https://www.sport-express.ru/hockey/L/khl/2024-2025/stadiums/', "Не правильный урл стадиона"
+    def date(self):
+        try:
+            date = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//p[@class='se-material-page__date']")))
+            logging.info("Дата и время найдены и отображаются")
+            return date
+        except TimeoutException:
+            return None
 
-    def table_stadium(self):
-        table_staium = self.browser.find_element(By.XPATH, '//div[@class="sp-page sp-page--overflow"]')
+    def bread(self):
+        try:
+            bread = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//a[@href='/hockey/']")))
+            logging.info("Крошки выводятся")
+            bread.click()
+            assert self.browser.current_url == 'https://www.sport-express.ru/hockey/', "Не правильный переход в хоебные крошки"
+            self.browser.back()
+            return bread
+        except TimeoutException:
+            return None
 
-    def stadium_click(self):
-        stadium_click = self.browser.find_element(By.XPATH, '//tbody/tr[7]/td[2]')
-        stadium_click.click()
-
-    def tag_stadium_check(self):
-        tag_stadium_check = self.browser.find_element(By.XPATH, '//div[@class="sp-profile-name__name"]')
-        assert self.browser.current_url == 'https://www.sport-express.ru/hockey/L/stadium/452/'
-
-    def h1_stadium(self):
-        h1_stadium = self.browser.find_element(By.XPATH, '//h1[contains(text(),"Стадионы")]')
-
-
+    def stadion(self):
+        try:
+            stadion = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH, "//div[@class='se-menu-subtop__link']")))
+            ActionChains(self.browser).move_to_element(stadion).perform()
+            logging.info("Курсор наведен на элемент меню")
+            stadion2 = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH, "//a[contains(text(),'Стадионы')]")))
+            stadion2.click()
+            sleep(2)
+            assert self.browser.current_url == 'https://www.sport-express.ru/hockey/L/khl/2024-2025/stadiums/', "Не правильная ссылка стадионов"
+            logging.info("Вторая кнопка найдена и нажата")
+            return stadion2
+        except TimeoutException:
+            return None
--- test/test_4667.py
+++ test/test_4667.py
@@ -1,14 +1,24 @@
 from pages.detalvideo import DetalVideo
-from selenium.webdriver.common.by import By
+
 
 def test_detalvideo(browser):
     detalvideo = DetalVideo(browser)
     detalvideo.open_detal_video()
-    detalvideo.menu_nadlogo()
-    detalvideo.podval()
-    detalvideo.eshe()
-    detalvideo.perexod_stadium()
-    detalvideo.h1_stadium()
-    detalvideo.table_stadium()
-    detalvideo.stadium_click()
-    detalvideo.tag_stadium_check()
+
+    menu = detalvideo.menu_nadlogo()
+    assert menu is not None, "Меню не выводится"
+
+    h1 = detalvideo.h1()
+    assert h1 is not None, "Заголовок не выводится"
+
+    reklama = detalvideo.rekalama()
+    assert reklama is not None, "Реклама не выводится"
+
+    datet = detalvideo.date()
+    assert datet is not None, "Дата не отображается"
+
+    bread = detalvideo.bread()
+    assert bread is not None, "Хлебные крошки не выводятся"
+
+    stadion2 = detalvideo.stadion()
+    assert stadion2 is not None, "Кнопка стадиона не найдена"
