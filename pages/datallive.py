f822acd3a261369351c0b6c8b66276168f329900
Nick <nick.khudakov@gmail.com>
2025-04-14 14:34:32 +0300

    детальные готово

--- pages/datallive.py
+++ pages/datallive.py
@@ -1,36 +1,63 @@
-from selenium.webdriver.common.by import By
 from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains
+import logging
 
 
 class DetalLive:
 
     def __init__(self, browser):
         self.browser = browser
+        self.wait = WebDriverWait(self.browser, 15)
+        self.original_window = None
 
     def open_detal_live(self):
         self.browser.get('https://www.sport-express.ru/football/rfpl/online/spartak-fakel-smotret-besplatno-match-20-tura-rpl-v-pryamom-efire-onlayn-tekstovaya-translyaciya-i-rezultat-10-marta-2024-2187914/')
-        sleep(6)
 
     def menu_nadlogo(self):
-        menu_nadlogo = self.browser.find_element(By.XPATH, '//div[@class="se-menu-subtop se-menu-subtop--breadcrumb"]')
+        try:
+            menu_nadlogo = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH, '//div[@class="se-menu-subtop se-menu-subtop--breadcrumb"]')))
+            logging.info("Меню найдено")
+            return menu_nadlogo
+        except TimeoutException:
+            return None
+
+    def h1(self):
+        try:
+            h1 = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//h1[contains(text(),'«Спартак» — «Факел»: воронежцы вновь обыграли крас')]")))
+            logging.info("Заголовок найден")
+            return h1
+        except TimeoutException:
+            return None
 
     def datelive(self):
-        detelive = self.browser.find_element(By.XPATH, '//div[contains(text(),"10 марта, 21:03")]')
+        try:
+            data = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,'//div[contains(text(),"10 марта, 21:03")]')))
+            logging.info("Время найдено")
+            return data
+        except TimeoutException:
+            return None
 
     def podval(self):
-        podval = self.browser.find_element(By.XPATH, '//footer[@class="se-footer"]')
+        try:
+            data = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,'//footer[@class="se-footer"]')))
+            logging.info("Подвал найдено")
+            return data
+        except TimeoutException:
+            return None
 
-    def dopknopkalive(self):
-        dopknopkalive = self.browser.find_element(By.XPATH,'//div[@class="se-menu-subtop__link"]')
-        dopknopkalive.click()
-
-    def videoclicklive(self):
-        videoclicklive = self.browser.find_element(By.XPATH, '''//div[@class='se-menu-subtop-popup__link']//a[contains(text(),"Видео")]''')
-        videoclicklive.click()
-        assert self.browser.current_url == 'https://www.sport-express.ru/football/rfpl/videoreports/', "Не правильный урл видео"
-
-    def h1_video(self):
-        h1_video = self.browser.find_element(By.XPATH, '//h1[contains(text(),"Видео: футбол РПЛ")]')
-
-    def items_video(self):
-        items_video = self.browser.find_element(By.XPATH, "//div[@class='se-video-list-page__items']")
+    def comand(self):
+        try:
+            comand = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//div[contains(text(),'Положение команд')]")))
+            logging.info("Блок положение команд найден")
+            return comand
+        except TimeoutException:
+            return None
--- test/test_4669.py
+++ test/test_4669.py
@@ -4,9 +4,18 @@ from selenium.webdriver.common.by import By
 def test_detallive(browser):
     detallive = DetalLive(browser)
     detallive.open_detal_live()
-    detallive.menu_nadlogo()
-    detallive.datelive()
-    detallive.podval()
-    detallive.dopknopkalive()
-    detallive.videoclicklive()
-    detallive.h1_video()
+
+    menu = detallive.menu_nadlogo()
+    assert menu is not None, "Меню не найдено"
+
+    h1 = detallive.h1()
+    assert h1 is not None, "Заголовок не найден"
+
+    date = detallive.datelive()
+    assert date is not None, "Дата и время не найдено"
+
+    comand = detallive.comand()
+    assert comand is not None, "Блок положение команд не найден"
+
+    footer = detallive.podval()
+    assert footer is not None, "Подвал не найден"
