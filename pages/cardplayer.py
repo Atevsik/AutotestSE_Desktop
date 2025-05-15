9ed2f6ef6d2f2ddaa163ff80e7e39b125f6f3559
Nick <nick.khudakov@gmail.com>
2025-05-15 17:23:44 +0300

    15.05.25card

--- pages/cardplayer.py
+++ pages/cardplayer.py
@@ -1,37 +1,124 @@
 from selenium.webdriver.common.by import By
-from time import sleep
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import logging
+from selenium.common.exceptions import NoSuchElementException
 
 
 class CardPlayer:
 
     def __init__(self, browser):
         self.browser = browser
+        self.wait = WebDriverWait(self.browser, 10)
+        self.original_window = None
 
     def open(self):
-        self.browser.get('https://www.sport-express.ru/football/L/player/2283/seasons/2023-2024/')
-        sleep(6)
+        self.browser.get('https://www.sport-express.ru/tag/krishtianu-ronaldu-973/')
 
-    def menu_nadlogo(self):
-        menu_nadlogo = self.browser.find_element(By.XPATH, '//div[@class="se-menu-subtop se-menu-subtop--breadcrumb"]')
+    def menu(self):
+        try:
+            menu = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//div[@class='se-menu-subtop se-menu-subtop--main']")))
+            logging.info("меню найдено")
+            return menu
+        except TimeoutException:
+            return None
 
-    def info(self):
-        info = self.browser.find_element(By.XPATH, "//table[@class='se19-table-player-card']")
+    def verify_all_elements(self):
+        elements = [
+            ("//td[contains(text(),'Вид спорта:')]", "вид спорта"),
+            ("//td[contains(text(),'Клуб:')]", "клуб"),
+            ("//td[contains(text(),'Возраст:')]", "возраст"),
+            ("//td[contains(text(),'Рост:')]", "рост"),
+            ("//td[normalize-space()='80']","Вес"),
+            ("//td[contains(text(),'5 февраля 1985')]", "дата"),
+            ("//td[contains(text(),'Португалия')]", "Гражданство"),
+            ("//td[contains(text(),'Амплуа:')]","Амплуа"),
+            ("//td[contains(text(),'Номер:')]","Номер")
+        ]
 
-    def match(self):
-        match = self.browser.find_element(By.XPATH,"//td[normalize-space()='26.03.2024']")
+        all_present = True
 
-    def click_match(self):
-        click_match = self.browser.find_element(By.XPATH,"//a[normalize-space()='0 : 3']")
-        click_match.click()
-        assert self.browser.current_url == 'https://www.sport-express.ru/football/euro/fbl_match-turciya-portugaliya-388571/', "Не правильный матч"
-        self.browser.back()
+        for xpath, description in elements:
+            try:
+                self.browser.find_element(By.XPATH, xpath)
+                logging.info(f"{description} - найден")
+            except NoSuchElementException:
+                logging.error(f"{description} - не найден")
+                all_present = False
+
+        return all_present
+
+    def bio(self):
+        try:
+            bio = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//div[@class='se-tags-player-description__text']")))
+            logging.info("Био найдено")
+            return bio
+        except TimeoutException:
+            return None
+
+    def click_info(self):
+        try:
+            click_info = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//u[contains(text(),'Футбол')]")))
+            logging.info("Текст футбол найден")
+            click_info.click()
+            assert self.browser.current_url == 'https://www.sport-express.ru/football/', "Не правильный урл"
+            self.browser.back()
+
+            click_info2 = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//u[contains(text(),'Аль-Наср')]")))
+            logging.info("Команда найдена")
+            click_info2.click()
+            assert self.browser.current_url == 'https://www.sport-express.ru/tag/fk-al-nasr-24643/', "Не правильный урл команды"
+            self.browser.back()
+            return click_info2
+        except TimeoutException:
+            return None
+
+    def button_click(self):
+        try:
+            btn = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//a[contains(text(),'Все о спортсмене')]")))
+            logging.info("Кнопка все о спортсмене найдена")
+            btn.click()
+            assert self.browser.current_url == 'https://www.sport-express.ru/football/L/player/2283/seasons/2024-2025/', "Не правильный урл все о спортсмене"
+            self.browser.back()
+
+            btn2 = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//a[contains(text(),'Биография')]")))
+            logging.info("Кнопка бион не найдена")
+            btn2.click()
+            assert self.browser.current_url == 'https://www.sport-express.ru/football/england/reviews/kratkaya-biografiya-krishtianu-ronaldu-detstvo-klubnaya-i-mezhdunarodnaya-karera-lichnaya-zhizn-1946597/',"не правильный урл био"
+            self.browser.back()
+            return btn2
+        except TimeoutException:
+            return None
 
     def reklama(self):
-        reklama = self.browser.find_element(By.XPATH,"//div[@id='adfox_15645683733586888']")
+        try:
+            reklama = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//div[@id='adfox_15645683733586888']")))
+            logging.info("Реклама найдена")
+            return reklama
+        except TimeoutException:
+            return None
 
-    def filtr(self):
-        filtr = self.browser.find_element(By.XPATH,"//div[@class='cusel_select mr_20']")
+    def next_page(self):
+        try:
+            next = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//a[normalize-space()='5']")))
+            logging.info("Кнопка страницы 5 найдена")
+            next.click()
+            assert self.browser.current_url == 'https://www.sport-express.ru/tag/krishtianu-ronaldu-973/page5/', "Не правильный урл страницы"
 
-
+            next2 = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//a[contains(text(),'Далее')]")))
+            logging.info("Кнопка далее найдена")
+            next2.click()
+            assert self.browser.current_url == 'https://www.sport-express.ru/tag/krishtianu-ronaldu-973/page6/', "Не рпавьный урл страницы 6"
+            return next2
+        except TimeoutException:
+            return None
