38ce05ac807252b692c2b13c8119d0bc967a90d8
Nick <nick.khudakov@gmail.com>
2025-05-15 14:43:46 +0300

    mma 15.05

--- pages/mmaraiting.py
+++ pages/mmaraiting.py
@@ -1,6 +1,10 @@
-from selenium.webdriver.common.by import By
 from time import sleep
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import logging
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import Select
 
 
@@ -8,59 +12,136 @@ class MmaRaiting:
 
     def __init__(self, browser):
         self.browser = browser
+        self.wait = WebDriverWait(self.browser, 10)
+        self.original_window = None
 
     def open(self):
         self.browser.get('https://www.sport-express.ru/fighting/mma/ufc/2024/ratings/men/')
-        sleep(6)
 
-    def menu_nadlogo(self):
-        menu_nadlogo = self.browser.find_element(By.XPATH, '//div[@class="se-menu-subtop se-menu-subtop--breadcrumb"]')
+    def menu(self):
+        try:
+            menu = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,'//div[@class="se-menu-subtop se-menu-subtop--breadcrumb"]')))
+            logging.info("Меню найдено")
+            return menu
+        except TimeoutException:
+            return None
 
     def h1(self):
-        h1 = self.browser.find_element(By.XPATH,"//div[@class='sp-sport-page__title']")
+        try:
+            h1 = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//div[@class='sp-sport-page__title']")))
+            logging.info("Заголовок найден")
+            return h1
+        except TimeoutException:
+            return None
 
-    def boets(self,count):
-        boets = self.browser.find_elements(By.XPATH,"//div[@class='sp-ratings__fighter-info']")
-        assert len(boets) == count
+    def boets(self, expected_count=15):
+        try:
+            player = self.wait.until(
+                EC.visibility_of_all_elements_located(
+                    (By.XPATH, "//div[@class='sp-ratings__fighter-info']")))
+            actual_count = len(player)
+            if actual_count == expected_count:
+                logging.info(f"Найдено {actual_count} бойцов (ожидалось {expected_count})")
+                return True
+            else:
+                logging.warning(f"Кол-во бойцов:{actual_count} (ожидалось {expected_count})")
+                return False
+        except TimeoutException:
+            logging.error("Не удалось загрузить бойцов")
+            return False
 
-    def champion(self):
-        champion = self.browser.find_element(By.XPATH,"//div[@class='se-fighter-champion-panel se-fighter-champion-panel--champion-mma']")
+    def champion_panel(self):
+        try:
+            champion = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//div[@class='se-fighter-champion-panel__left']")))
+            logging.info("Панель чемпиона найдена")
+            return champion
+        except TimeoutException:
+            return None
 
-    def stolb(self,count):
-        stolb = self.browser.find_elements(By.XPATH,"//th[@class='se-advanced-table-cell']")
-        assert len(stolb) == count
+    def click_fighter(self):
+        try:
+            click1 = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//a[contains(text(),'Александр Пантожа')]")))
+            logging.info("Чемпион найден")
+            click1.click()
+            assert self.browser.current_url == 'https://www.sport-express.ru/tag/aleksandr-pantozha-25211/', "Не правильный урл чемпиона"
+            self.browser.back()
 
-    def japan(self):
-        japan = self.browser.find_element(By.XPATH,"//a[contains(text(),'Тацуро Тайра')]")
-        japan.click()
-        assert self.browser.current_url == 'https://www.sport-express.ru/tag/tacuro-tayra-26076/', "Не правильный урл бойца"
-        self.browser.back()
+            click2 = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//a[contains(text(),'Брэндон Ройвал')]")))
+            logging.info("Боец найден")
+            click2.click()
+            assert self.browser.current_url == 'https://www.sport-express.ru/tag/brendon-royval-25214/', "Не правильный урл бойца"
+            self.browser.back()
+            return click2
+        except TimeoutException:
+            return None
 
-    def prom(self):
-        prom = self.browser.find_element(By.XPATH,"//div[@class='se-sport-navigator__competition']")
-        prom.click()
-        sleep(3)
 
-    def prom1(self):
-        prom1 = self.browser.find_element(By.CSS_SELECTOR,"#react-select-competitions-option-0-4")
-        prom1.click()
-        assert self.browser.current_url == 'https://www.sport-express.ru/fighting/mma/aca/2025/calendar/', "Не правильный урл АСА"
+    def verify_all_elements(self):
+        elements = [
+            ("//div[contains(@class,'asc')]", "Заголовок номер "),
+            ("//div[contains(text(),'Боец')]", "Заголовок боец"),
+            ("//div[contains(text(),'Рекорд в промоушене')]", "Рекорд"),
+            ("//div[contains(text(),'Общий рекорд')]", "Общий рекорд")
+        ]
+
+        all_present = True
+
+        for xpath, description in elements:
+            try:
+                self.browser.find_element(By.XPATH, xpath)
+                logging.info(f"{description} - найден")
+            except NoSuchElementException:
+                logging.error(f"{description} - не найден")
+                all_present = False
+
+        return all_present
+
+    def sort(self):
+        try:
+            sort1 = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//div[contains(@class,'asc')]")))
+            logging.info("Кнопка номера найдена")
+            sort1.click()
+
+            sort2 = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//div[contains(@class,'sp-ratings__positions_number')][normalize-space()='1']")))
+            logging.info("Сортировка работает")
+            return sort2
+        except TimeoutException:
+            return None
+
+    def select_aca(self):
+        try:
+            select = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//div[@class='se-sport-navigator__competition']")))
+            logging.info("Селектор переключения найден")
+            select.click()
+
+            select2 = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//div[@id='react-select-competitions-option-0-4']")))
+            logging.info("Нужный турнир найден")
+            select2.click()
+            assert self.browser.current_url == 'https://www.sport-express.ru/fighting/mma/aca/2025/calendar/', "Не правильный урл промоушена"
+
+            select3 = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH,"//div[@class='se-page-menu-item']//a[contains(text(),'Рейтинги')]")))
+            logging.info("Кнопка рейтинги найдена")
+            select3.click()
+            assert self.browser.current_url == 'https://www.sport-express.ru/fighting/mma/aca/2025/ratings/men/', "Не правильная ссылка рейтингов"
+            return select3
+        except TimeoutException:
+            return None
 
     def reklama(self):
-        reklama = self.browser.find_element(By.XPATH,"//div[@id='adfox_15645683733586888']")
+        try:
+            reklama = self.wait.until((By.XPATH,"//div[@id='adfox_15645683733586888']")))
+            logging.info("Реклама найдена")
+            return reklama
+        except TimeoutException:
+            return None
 
-    def seson(self):
-        seson = self.browser.find_element(By.XPATH,"//div[@class='se-sport-navigator__season']")
-        seson.click()
-
-    def seson2(self):
-        seson2 = self.browser.find_element(By.XPATH,"//div[@id='react-select-seasons-option-0-1']")
-        seson2.click()
-        assert self.browser.current_url == 'https://www.sport-express.ru/fighting/mma/aca/2024/calendar/', "Не правильный год"
-
-    def raitingi(self):
-        raitingi = self.browser.find_element(By.XPATH,"//div[@class='se-page-menu-item']//a[contains(text(),'Рейтинги')]")
-        raitingi.click()
-
-    def dopprover(self):
-        dopprover = self.browser.find_element(By.XPATH,"//a[contains(text(),'Юнус Евлоев')]")
